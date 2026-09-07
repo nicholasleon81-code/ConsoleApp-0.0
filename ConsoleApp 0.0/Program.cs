3f72d48b7c1be4802e2d9e6c20d9d1b7ce2662b8
nicho <nicho@NICHOLASLAPTOP>
2026-09-07 09:42:33 +0200

    Add project files.

---
+++ ConsoleApp 0.0/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Enhetsregister");
+Console.WriteLine();
+Console.WriteLine("ID D-100 | Namn: Laptopsal 1 | Status: Aktiv");
+Console.WriteLine("ID D-200 | Namn: Projektor 1 | Status: Service");
