c7e1afde41eead5031cda11ead9ac3679f638420
nicholasleon81@gmail.com <nicholasleon81@gmail.com>
2026-09-07 10:07:54 +0200

    Added new items to Registery

--- ConsoleApp 0.0/Program.cs
+++ ConsoleApp 0.0/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine();
 Console.WriteLine("ID D-100 | Namn: Laptopsal 1 | Status: Aktiv");
 Console.WriteLine("ID D-200 | Namn: Projektor 1 | Status: Service");
+Console.WriteLine("ID D-300 | Namn: Skärm 1 | Status: Inaktiv");
