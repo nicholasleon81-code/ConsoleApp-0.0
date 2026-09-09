fc7e594dc401eaa12f7e86bcd0ecf0ba04ef1445
Fridaychill <nicholasleon81@gmail.com>
2026-09-09 15:48:21 +0200

    moms added

--- ConsoleApp 0.0/Program.cs
+++ ConsoleApp 0.0/Program.cs
@@ -1,7 +1,10 @@
-﻿string enhet1 = "Laptopsal 1";
+﻿double moms = 0.25;
+
+string enhet1 = "Laptopsal 1";
 string id1 = "D-100";
 string status1 = "Aktiv";
 int price1 = 250;
+double brutto1 = price1 * (1 + moms);
 
 string enhet2 = "Projektor 1";
 string id2 = "D-200";
@@ -13,16 +16,14 @@ string enhet3 = "Skärm 1";
 string status3 = "Inaktiv";
 int price3 = 800;
 
-double moms = 0.25;
-
 // Det här är rad 2 i vårat register (ifall du undrar)
-string row2 = "ID: " + id2 + " | Namn: " + enhet2 + " | " + "Status: " + status2 + " | Pris (Brutto): " + price2.ToString();
+string row2 = "ID: " + id2 + " | Namn: " + enhet2 + " | " + "Status: " + status2 + " | Pris: " + price2 + " | Brutto: " + price2 * (1 + moms);
 
-string row3 = $"ID:  {id3} | Namn: {enhet3} | Status: {status3}";
+string row3 = $"ID:  {id3} | Namn: {enhet3} | Status: {status3} | Pris: {price3} | Brutto: {price3 * (1 + moms)}";
 
 Console.WriteLine("Enhetsregister");
 Console.WriteLine();
-Console.WriteLine("ID: " + id1 + " | Namn: " + enhet1 + " | Status: " + status1 + " | Pris(brutto): " + price1);
+Console.WriteLine("ID: " + id1 + " | Namn: " + enhet1 + " | Status: " + status1 + " | Pris: " + price1 + " Brutto: " + price1 * (1 + moms));
 Console.WriteLine(row2);
 Console.WriteLine(row3);
 
@@ -38,8 +39,11 @@ string status4 = Console.ReadLine();
 Console.WriteLine("Ange pris: ");
 int price4 = Convert.ToInt32(Console.ReadLine());
 
-string row4 = $"ID: {id4} | Namn: {enhet4} | Status: {status4} | Pris: {price4} kr";
+string row4 = $"ID: {id4} | Namn: {enhet4} | Status: {status4} | Pris: {price4} kr | Brutto: {price4 * (1 + moms)} kr";
 
 Console.WriteLine();
 Console.WriteLine("Ny enhet: ");
 Console.WriteLine(row4);
+
+int totalPrice = price1 + price2 + price3 + price4;
+Console.WriteLine(totalPrice);
